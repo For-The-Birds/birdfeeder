0a75ee1468a5289acf18d1a5bf0d7152671e4626
Pavel V <pavel@qfactor.me>
2020-05-07 13:29:35 +0300

    startup script

---
+++ runall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function t {
+    pkill -fe dslr.sh
+    pkill -fe motion
+    pkill -fe flask
+}
+
+trap t SIGINT SIGTERM EXIT
+
+cd $(dirname $0)
+FLASK_APP=pred.py python3 -m flask run &
+motion -c motion.conf
+bash dlsr.sh
+tail -f motion.log
+
