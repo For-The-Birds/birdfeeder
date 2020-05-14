b145dc3233767191c3e06356e930b9cefedd8ad2
Pavel V <pavel@qfactor.me>
2020-05-14 15:48:04 +0300

    include time of day

--- motion-detected.sh
+++ motion-detected.sh
@@ -10,7 +10,7 @@ fi
 if [ $1 = "end" ] ; then
     read st <start_time
     duration=$( echo "$(date +%s) - $st" | bc )
-    echo $(date --date=@$st) _ $st _ $duration | tee -a motions.txt
+    echo $(date --date=@$st) _ $st _ $(date +%R) _ $duration | tee -a motions.txt
     rm -v motion-detected
 fi
 
